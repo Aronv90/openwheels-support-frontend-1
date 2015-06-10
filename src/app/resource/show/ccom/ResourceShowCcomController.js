25a345470dee5c6f0cba63144bf835446a4a031a
Aron Vaas <aron@mywheels.nl>
2015-06-10 08:39:26 +0200

    Loop trough alarm descriptions to define the filter options

--- src/app/resource/show/ccom/ResourceShowCcomController.js
+++ src/app/resource/show/ccom/ResourceShowCcomController.js
@@ -5,6 +5,4 @@ angular.module('openwheels.resource.show.ccom', [])
 .controller('ResourceShowCcomController', function ($scope, alarms, $filter) {
   $scope.alarms = alarms;
 
-  $scope.alarmDescriptions = ['Uni restarted after empty batery', 'Unit restarted after plug of (current was off)', 'Warning. Battery is almost empty'];
-
 });
