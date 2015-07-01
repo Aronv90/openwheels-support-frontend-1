81aa9e10a6093a0bb9d1b9d32b7fd6d29d9ee333
Jorrit den Broeder <jorrit@parksound.nl>
2015-07-01 14:37:09 +0200

    feature: search person by IBAN number [CON-110]

--- src/app/person/search/PersonSearchController.js
+++ src/app/person/search/PersonSearchController.js
@@ -2,16 +2,32 @@
 
 angular.module('openwheels.person.search', [])
 
-    .controller('PersonSearchController', function ($scope, personService) {
-    
-        $scope.searchZip = function (person) {
-            return personService.searchZip({
-                zipcode: person.zipcode,
-                streetNumber: person.streetNumber
-              })
-            .then(function (persons) {
-                $scope.persons = persons;
-              });
-          };
-          
-      });
+.controller('PersonSearchController', function ($scope, personService, alertService) {
+
+  $scope.data = {};
+
+  $scope.searchZip = function () {
+    alertService.load();
+    personService.searchZip({
+      zipcode: $scope.data.zipcode,
+      streetNumber: $scope.data.streetNumber
+    })
+    .then(function (persons) {
+      $scope.persons = persons;
+    })
+    .catch(alertService.addError)
+    .finally(alertService.loaded);
+  };
+
+  $scope.searchIban = function () {
+    alertService.load();
+    personService.search({
+      iban: $scope.data.iban
+    })
+    .then(function (persons) {
+      $scope.persons = persons;
+    })
+    .finally(alertService.loaded);
+  };
+
+});
