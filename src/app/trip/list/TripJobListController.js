eaf32076568bdee9695f066baa764a52e54e225c
Bram Tervoort <b.tervoort@gmail.com>
2015-09-21 14:26:12 +0200

    bug gemaakt.

--- src/app/trip/list/TripJobListController.js
+++ src/app/trip/list/TripJobListController.js
@@ -3,26 +3,30 @@
 angular.module('openwheels.trip.list', [])
 .controller('TripJobsListController', function ($scope, jobs, eventSourceService, ccomeService){
   function update_job(event) {
-    var eventData = JSON.parse(event.data);
-    var findPos = function (eventData) {
-      for(var key in $scope.jobs) {
-        if($scope.jobs[key].job.id === eventData.job_id) {
-          return key;
-        }
-      }
-    };
-    
-    var key = findPos(eventData);
+    var eventData = JSON.parse(event.data),
+        findPos = function (eventData) {
+          for(var key in $scope.jobs) {
+            if($scope.jobs[key].job.id === eventData.job_id) {
+              return key;
+            }
+          }
+          return false;
+        },
+        key = findPos(eventData);
+
     if(key) {
       if(eventData.job_status === 'success') {
         $scope.jobs[key].job.status = 'success';
         delete $scope.jobs[key];
-      } else {
-        ccomeService.getState({state: $scope.jobs[key].id})
-        .then(function (data) {
-          $scope.jobs[key] = data;
-        });
+        
+        return;
       }
+      
+      ccomeService.getState({state: $scope.jobs[key].id})
+      .then(function (data) {
+        $scope.jobs[key] = data;
+      });
+      
       return;
     }
     
