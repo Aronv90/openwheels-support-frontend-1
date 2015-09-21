b3254dc313fe233cf460938e84055e3e63dea7a7
Bram Tervoort <b.tervoort@gmail.com>
2015-09-21 10:50:20 +0200

    fix job list compiled nu

--- src/app/trip/list/TripJobListController.js
+++ src/app/trip/list/TripJobListController.js
@@ -4,21 +4,28 @@ angular.module('openwheels.trip.list', [])
 .controller('TripJobsListController', function ($scope, jobs, eventSourceService, ccomeService){
   function update_job(event) {
     var eventData = JSON.parse(event.data);
-    for(var key in $scope.jobs) {
-      if($scope.jobs[key].job.id === eventData.job_id) {
-        if(eventData.job_status === 'success') {
-          $scope.jobs[key].job.status = 'success';
-          
-          delete $scope.jobs[key];
-        } else {
-          ccomeService.getState({state: $scope.jobs[key].id})
-          .then(function (data) {
-            $scope.jobs[key] = data;
-          });
+    var findPos = function (eventData) {
+      for(var key in $scope.jobs) {
+        if($scope.jobs[key].job.id === eventData.job_id) {
+          return key;
         }
-        return;
       }
+    };
+    
+    var key = findPos(eventData);
+    if(key) {
+      if(eventData.job_status === 'success') {
+        $scope.jobs[key].job.status = 'success';
+        delete $scope.jobs[key];
+      } else {
+        ccomeService.getState({state: $scope.jobs[key].id})
+        .then(function (data) {
+          $scope.jobs[key] = data;
+        });
+      }
+      return;
     }
+    
     ccomeService.getState({state: $scope.jobs[key].id})
     .then(function (data) {
       $scope.jobs.push(data);
