ff1ddde52680bfc74530bf0ec3960cea53db938c
Bram Tervoort <b.tervoort@gmail.com>
2015-10-13 10:36:19 +0200

    fix een beetje schonere code.

--- src/app/checklist/dashboard/dashboard.js
+++ src/app/checklist/dashboard/dashboard.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('openwheels.checklist.dashboard', ['googlechart'])
-.controller('DashboardController', function ($scope, bookingService, ccomeService, $interval, googleChartApiPromise) {
+.controller('DashboardController', function ($scope, bookingService, ccomeService, $interval, googleChartApiPromise, $q) {
   var doUpdate = function () {
     bookingService.futureByNotActiveDriver().then(function (list) {
       $scope.notActiveDrivers = list;
@@ -14,38 +14,37 @@ angular.module('openwheels.checklist.dashboard', ['googlechart'])
     ccomeService.unfinishedJobs().then(function(list){
       $scope.unfinishedJobs = list;
     });
+  };  
+  $scope.chartData = {
+    'type': 'AreaChart',
+    'displayed': true,
+    'data': [],
+    'options': {
+      'title': 'ritten per week',
+      'isStacked': 'true',
+      'displayExactValues': true,
+      'vAxis': {
+        'title': 'Ritten',
+        'gridlines': {
+          'count': 5
+        }
+      },
+      'hAxis': {
+        'title': 'Week'
+      }
+    }
   };
   
-  bookingService.bookingWeekKPIData().then(function(list) {
-    googleChartApiPromise.then(function () {
-      var data = new window.google.visualization.DataTable();
-      $scope.chartData = {
-        'type': 'AreaChart',
-        'displayed': true,
-        'data': data,
-        'options': {
-          'title': 'ritten per week',
-          'isStacked': 'true',
-    //      'fill': 20,
-          'displayExactValues': true,
-          'vAxis': {
-            'title': 'Ritten',
-            'gridlines': {
-              'count': 5
-            }
-          },
-          'hAxis': {
-            'title': 'Week'
-          }
-        }
-      };
-      data.addColumn('number', 'week');
-      data.addColumn('number', 'eigen autos');
-      data.addColumn('number', 'pear 2 pear autos');
-      
-      list.forEach(function(item) {
-        data.addRow([item['weekNr'], parseInt(item['rittenEigenAutos']), parseInt(item['rittenP2pAutosS'])]);
-      });
+  $q.all([bookingService.bookingWeekKPIData(), googleChartApiPromise])
+  .then(function(result) {
+    var list = result[0];
+    var data = $scope.chartData = new window.google.visualization.DataTable();
+    data.addColumn('number', 'week');
+    data.addColumn('number', 'eigen autos');
+    data.addColumn('number', 'pear 2 pear autos');
+    
+    list.forEach(function(item) {
+      data.addRow([item['weekNr'], parseInt(item['rittenEigenAutos']), parseInt(item['rittenP2pAutosS'])]);
     });
   });
   
