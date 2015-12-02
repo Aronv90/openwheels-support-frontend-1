7b4b8bbf98c526d495added4aab2b5f6bf05ecbb
Jorrit den Broeder <jorrit@parksound.nl>
2015-12-02 14:38:04 +0100

    WIP editable tripdata booking-id #1876

--- src/app/resource/show/tripdata/ResourceShowTripdataController.js
+++ src/app/resource/show/tripdata/ResourceShowTripdataController.js
@@ -2,16 +2,18 @@
 
 angular.module('openwheels.resource.show.tripdata', ['infinite-scroll'])
 
-.controller('ResourceShowTripdataController', function ($scope, $log, boardcomputerService) {
+.controller('ResourceShowTripdataController', function ($modal, $scope, $log, API_DATE_FORMAT,
+  alertService, boardcomputerService, resource) {
+
   var loading = false,
       offset = 0,
       limit = 20;
-      
+
   $scope.records = [];
   $scope.moreData = function () {
     if(!loading){
       $log.debug('load more tripdata');
-      
+
       loading = boardcomputerService.tripdata({
         resource: $scope.resource.id,
         limit: limit,
@@ -26,4 +28,61 @@ angular.module('openwheels.resource.show.tripdata', ['infinite-scroll'])
       });
     }
   };
+
+  $scope.showEditPopup = function (record) {
+    $modal.open({
+      templateUrl: 'resource/show/tripdata/popup.tpl.html',
+      controller: ['$scope', '$modalInstance', 'bookingService', function ($scope, $modalInstance, bookingService) {
+
+        $scope.entry = angular.copy(record);
+        $scope.close = $modalInstance.close;
+        $scope.suggestedBookings = [];
+
+        loadSuggestedBookings();
+
+        // TODO
+        $scope.save = function () {
+          // alertService.load();
+          // tripDataService.alter({
+          //   id: record.id,
+          //   newProps: {
+          //     bookingId: $scope.entry.bookingId
+          //   }
+          // })
+          // .then(function (altered) {
+          //   angular.extend(record, $scope.entry);
+          //   $modalInstance.close();
+          // })
+          // .catch(function (err) {
+          //   alertService.add('danger', error.message, 5000);
+          // })
+          // .finally(function () {
+          //   alertService.loaded();
+          // });
+        };
+
+        function loadSuggestedBookings() {
+          var t1 = moment(record.startDatetime).subtract(1, 'years');
+          var t2 = moment(record.endDatetime).add(1, 'hours');
+
+          alertService.load();
+
+          bookingService.forResource({
+            resource: resource.id,
+            timeFrame: {
+              startDate: t1.format(API_DATE_FORMAT),
+              endDate  : t2.format(API_DATE_FORMAT)
+            }
+          })
+          .then(function (bookings) {
+            $scope.suggestedBookings = bookings;
+          })
+          .finally(function () {
+            alertService.loaded();
+          });
+        }
+      }]
+    });
+  };
+
 });
