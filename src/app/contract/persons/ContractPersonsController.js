f2cb1d4d3d36ff7d5a9579e283facc83624a73c4
Kelley van Evert <hello@kelleyvanevert.nl>
2019-02-20 15:20:35 +0100

    extra bestuurders op contract in backoffice; nog te testen met API fixes

--- src/app/contract/persons/ContractPersonsController.js
+++ src/app/contract/persons/ContractPersonsController.js
@@ -3,7 +3,11 @@
 angular.module('openwheels.contract.persons', [])
 
 .controller('ContractPersonsController', function ($scope, $filter, $uibModalInstance, dialogService, alertService,
-                                                   contractService, personService, contract, person) {
+  extraDriverService,
+  $log,
+  $q,
+  contractService, personService, contract, person
+) {
 
     $scope.contract = contract;
     $scope.person = person;
@@ -38,34 +42,114 @@ angular.module('openwheels.contract.persons', [])
       $uibModalInstance.dismiss();
     };
 
-    $scope.addPerson = function (newPerson) {
-      contractService.addPerson({id: contract.id, person: newPerson.id})
-        .then(function (updatedContract) {
-          $scope.contract.persons = updatedContract.persons;
-          alertService.add('success', 'Person added', 2000);
-        }, function (error) {
-          alertService.add('danger', error.message, 5000);
+    function possiblyUnwrapResult (inviteRequests) {
+      if (inviteRequests.result && _.isArray(inviteRequests.result)) {
+        inviteRequests = inviteRequests.result;
+      }
+      return inviteRequests;
+    }
+
+    $scope.loadingInviteRequests = true;
+    $scope.loadInviteRequests = function () {
+      return $q(function (resolve, reject) {
+        $scope.loadingInviteRequests = true;
+
+        extraDriverService.getRequestsForContract({
+          contract: contract.id
+        })
+        .then(possiblyUnwrapResult)
+        .then(function (inviteRequests) {
+          $scope.inviteRequests = inviteRequests.map(function (req) {
+            req.newStatus = req.status;
+            return req;
+          });
+          $scope.loadingInviteRequests = false;
+          resolve();
+        })
+        .catch(function (err) {
+          alertService.addError(err);
+          reject();
         });
+      });
     };
 
-    $scope.removePerson = function (person) {
-      var idx;
-      idx = $scope.contract.persons.indexOf(person);
+    $scope.inviteRequestStatuses = [
+      { value: 'invited',  label: 'Uitgenodigd' },
+      { value: 'declined', label: '├ Afgewezen' },
+      { value: 'accepted', label: '└ Geaccepteerd' },
+      { value: 'revoked',  label: '. ├ Ingetrokken' },
+      { value: 'removed',  label: '. └ Verwijderd' },
+    ];
 
-      dialogService.showModal({}, {
-        closeButtonText: 'Cancel',
-        actionButtonText: 'OK',
-        headerText: 'Are you sure?',
-        bodyText: 'Do you really want to remove ' + person.firstName + ' ' + person.surname +  '?'
+    $scope.acceptNewPerson = true;
+    
+    $scope.addInviteRequest = function (newPerson) {
+      var accept = $scope.acceptNewPerson;
+
+      extraDriverService.invitePersonForContract({
+        contract: contract.id,
+        email: newPerson.email,
       })
-      .then(function(){
-        return contractService.removePerson({id: contract.id, person: contract.persons[idx].id});
+      .then(function (addedPerson) {
+        $scope.loadInviteRequests()
+        .then(function () {
+          alertService.add('success', 'Person added', 2000);
+
+          if (accept) {
+
+            // find the new inviteRequest
+            var inviteRequest = _.find($scope.inviteRequests, function (inviteRequest) {
+              return inviteRequest.status === 'invited' && (inviteRequest.recipient.id === addedPerson.id);
+            });
+
+            if (!inviteRequest) {
+              alertService.addError({
+                message: 'Kon de nieuw-toegevoegde invite request niet vinden ?!'
+              });
+              return;
+            }
+
+            // try to accept it
+            $scope.updateInviteRequestStatus(inviteRequest, 'accepted');
+          }
+        });
       })
-      .then(function () {
-        $scope.contract.persons.splice(idx, 1);
-        alertService.add('success', 'Person removed', 2000);
-      }, function (error) {
+      .catch(function (error) {
         alertService.add('danger', error.message, 5000);
+      });
+    };
+
+    $scope.updateInviteRequestStatus = function (inviteRequest, newStatus) {
+      newStatus = newStatus || inviteRequest.newStatus;
+      var promise;
+
+      switch (newStatus) {
+        case 'declined': {
+          promise = extraDriverService.declineContractRequest({ id: inviteRequest.id });
+          break;
+        }
+        case 'accepted': {
+          promise = extraDriverService.acceptContractRequest({ id: inviteRequest.id });
+          break;
+        }
+        case 'revoked': {
+          promise = extraDriverService.revokeContractRequest({ id: inviteRequest.id });
+          break;
+        }
+        default: /*case 'removed':*/ {
+          promise = extraDriverService.removePersonFromContract({ contract: contract.id, person: inviteRequest.recipient.id });
+          break;
+        }
+      }
+
+      promise
+      .then(function (resp) {
+        $log.log('api response:', resp);
+        $scope.loadInviteRequests();
+      })
+      .catch(function (err) {
+        inviteRequest.newStatus = inviteRequest.status; // revert change
+        alertService.addError(err);
       });
     };
 
