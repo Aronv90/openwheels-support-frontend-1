483deaba27c4ad5c3761afe421fbe8026e888741
Aron <aron@mywheels.nl>
2019-10-07 14:52:01 +0200

    Undo

--- Gruntfile.js
+++ Gruntfile.js
@@ -373,7 +373,7 @@ module.exports = function (grunt) {
 
     connect: {
       options: {
-        port: 9000,
+        port: 9001,
         // Change this to '0.0.0.0' to access the server from outside.
         hostname: '0.0.0.0',
         livereload: 35729
