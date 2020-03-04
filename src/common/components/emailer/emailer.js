59561cac35945b3bab5209bdb05c226ad534c3a2
Aron <aron@mywheels.nl>
2020-03-04 17:09:41 +0100

    Update backoffice

--- src/common/components/emailer/emailer.js
+++ src/common/components/emailer/emailer.js
@@ -50,15 +50,15 @@ angular.module('openwheels.components')
             "het MyWheels Team",
             "Hans Rombout",
             "Daanjan Wisselink",
-            "Daan Hartog",
+            "Aida Kopmels",
             "Aron Vaas",
           ];
 
           var feedbackUrls = {
             "het MyWheels Team": "",
             "Hans Rombout": "https://docs.google.com/forms/d/e/1FAIpQLScWmFKXYxb_rRAQBKjaA_RPHYODTjWaDhC9qdoHEJjvMdxSxw/viewform?usp=sf_link",
-            "Daan Hartog": "https://docs.google.com/forms/d/e/1FAIpQLSfIG5eQbZ5MW14cE6VpvKWypscPFcy9OJmZNzl90CT9pI7BSA/viewform?usp=sf_link",
             "Daanjan Wisselink": "https://docs.google.com/forms/d/e/1FAIpQLSfGFL6RVIyhqZqR2dMV2JwDqjKcxZxCUd8Hvr66A-g6WLEvcQ/viewform?usp=sf_link",
+            "Aida Kopmels": "https://forms.gle/pDfx5tY3n5iafHnt6",
             "Aron Vaas": "",
           };
 
